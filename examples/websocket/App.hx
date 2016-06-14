802c7de7eac3aa136d9821d02e9fa6cda481fd6c
tong <tong@disktree.net>
2016-06-14 22:15:20 +0200

    websocket example fix

--- examples/websocket/App.hx
+++ examples/websocket/App.hx
@@ -8,22 +8,31 @@ class App {
 
 		var btn = document.getElementById( 'connect' );
 		btn.onclick = function() {
-			var socket = new js.html.WebSocket( 'ws://127.0.0.1:7700' );
+
+			var ip = untyped document.getElementById( "ip" ).value;
+			var port = untyped document.getElementById( "port" ).value;
+			var url = 'ws://$ip:$port';
+
+			trace( 'Connecting to $url ...' );
+
+			var socket = new js.html.WebSocket( url );
 			socket.onopen = function () {
 				console.debug( 'onopen' );
-				socket.send( "abc" );
+				//socket.send( "abc" );
 			};
 			socket.onerror = function(e) {
-				console.error(e);
+				console.error( 'onerror '+e);
 			};
 			socket.onclose = function(e) {
-				console.info(e);
+				console.info( 'onclose '+e );
 			};
 			socket.onmessage = function(e) {
-				console.info(e);
-				var message = document.createDivElement();
-				message.textContent = e.data;
-				document.body.appendChild( message );
+
+				console.info( 'onmessage '+e );
+
+				//var message = document.createDivElement();
+				//message.textContent = e.data;
+				//document.body.appendChild( message );
 			};
 		}
 	}
